9c47497dab3caa17019301b8549f11786185592d
Karan Shah <karanshah1561998@gmail.com>
2024-10-16 11:25:55 -0400

    Done DP-10|S30_HW56_20241014

---
+++ SuperEggDrop.java
@@ -0,0 +1,19 @@
+// Problem 887. Super Egg Drop
+// Space Complexity : O(n*k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int superEggDrop(int k, int n) {
+        int [][] dp = new int[n+1][k+1];
+        int attempts = 0;
+        while(dp[attempts][k] < n){
+            attempts++;
+            for(int j = 1; j <= k; j++){
+                dp[attempts][j] = 1 + dp[attempts - 1][j-1] + dp[attempts - 1][j];
+            }
+        }
+        return attempts;
+    }
+}
